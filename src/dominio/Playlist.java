6d444b74ba5339c626d191d5cd526632a94b7e53
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2024-11-20 16:51:27 -0300

    First Commit

---
+++ src/dominio/Playlist.java
@@ -0,0 +1,9 @@
+package dominio;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Playlist {
+    private String nome;
+    private List<Musica> musicas = new ArrayList<>();
+}
